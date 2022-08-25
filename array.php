2bde54ae645fed4059f6bfa768e164d302ecb516
aldyhaha <aldisafrudin64@gmail.com>
2022-08-25 13:28:54 +0700

    telah menyelesaikan array

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+
+// $warna = array("Merah", "Kuning", "Hijau");
+// $warna2 = ["Merah", "Kuning", "Hijau"];
+// echo " saya suka ", $warna2[1];
+// $produk = [
+//     "nama"=> "appel",
+//     "harga"=> 'Rp.2000',
+//     "total"=> "80"
+// ];
+
+// echo $produk ["total"];
+
+$data_barang = [
+    ["barang" => "Apel", "harga" => "2000", "stok" => "999"],
+    ["barang" => "Jeruk", "harga" => "2000", "stok" => "999"],
+];
+  print_r($data_barang[1]["barang"]);
+
+
+?>
